3d0ec64a1e0db74d74c4fd2420d89475c56b93b4
Ianfeather <info@ianfeather.co.uk>
2014-03-10 23:49:30 +0000

    Update component.js

--- javascript-component/templates/component.js
+++ javascript-component/templates/component.js
@@ -11,7 +11,7 @@ define([ "jquery" ], function($) {
   // @args = {}
   // el: {string} selector for parent element
   var <%= constructorName %> = function(args) {
-    this.$listener = $("#js-row--content" || args.$listener);
+    this.$listener = $(args.listener || "#js-row--content");
     this.$el = $(args.el);
     this.$el && this.init();
   }, _this;
